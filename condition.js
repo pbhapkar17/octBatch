140910d423c781605831eb85dd7bb922afc1c644
pbhapkar17 <poonambhapkar34@gmail.com>
2023-12-09 11:21:07 +0530

    opr

--- condition.js
+++ condition.js
@@ -1,5 +1,5 @@
 
-var marks = 85;
+var marks = 5;
 
 //if
 if ( marks > 75 ) {
@@ -23,6 +23,94 @@ if (marks >= 75) {
 } else if(marks >= 35 && marks < 50) {
     console.log("passed");
 }
-else {
-    console.log("fail");
+else{
+    console.log("fail!!!");
+}
+
+let amount = 100;
+
+switch (amount) {
+    case 50:
+        console.log("you have 50rs");
+        break;
+    case 60:
+        console.log("you have 60rs");
+        break;
+    case 80:
+        console.log("you have 80rs");
+        break;
+    case 100:
+        console.log("you have 100rs");
+         break;
+    case 90:
+        console.log("you have 90rs");
+        break;
+    default:
+        console.log("u hv money ");
+      
+}
+
+
+let a = 90; //assgment opr
+
+b = 20;
+
+if(b == "80"){  //== equality opr : it will chk only value not data type
+console.log("b has 80");
+}
+
+if( b === "80") {   //=== equality opr : it chks for value as well as data type
+console.log("bjhbhjbjh");
+}
+
+
+if (b > 20) {
+    console.log("jkjkjk");
+}
+if (b < 20) {
+    console.log("jkjkjk");
+}
+
+if (b == 20) {
+    console.log("jkjkjk");
+}
+
+if (b >= 20) {
+    console.log("jkjkjk");
+}
+if (b <= 20) {
+    console.log("jkjkjk");
+}
+
+if (b != 20) {
+    console.log("jkjkjk");
+}
+
+let c = true;
+if (c) {
+  console.log('bhjhj');  
+}
+
+if (!c) {  //!false>>true , !true>> false
+    console.log("!!!");
+}
+
+
+
+let d = 90;
+if (d) {
+    console.log('dddddddd');
+}
+
+if (!d) {
+    console.log('dddddddd');
+}
+
+let e = null;   //undefined/null/false>>false
+if (e) { 
+    console.log('eeeeeeeee');
+}
+
+if (!e) { 
+    console.log('eeeeeeeee');
 }
