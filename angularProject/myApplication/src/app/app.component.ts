cfa3335fe0508df71b409d52e8f14937c8965ba1
pbhapkar17 <poonambhapkar34@gmail.com>
2023-12-25 19:44:30 +0530

    db

--- angularProject/myApplication/src/app/app.component.ts
+++ angularProject/myApplication/src/app/app.component.ts
@@ -9,10 +9,25 @@ export class AppComponent {
   //let a = 90;
   title = 'myApplication';
   //propertyName : data type = value;
-
   a = 20;
   b: number = 90;
   c!: number;
+  city: string = "Pune";
+  //1. one way db 2. two way db
+  //1. one way db
+  //1.string interpolation>>{{}} >>syntax {{propertyName}}
+  //2.property binding >>[]>> [attribute]="proprtyName"
+  //3. event binding>>()>>on event will bind data to the property
+  //Two way data bind >> [()]>> [(ngModel)]="propertyName"
+  myName = "Poonam";
+  isDisable = false;
+  fruit!:string;
+
+  constructor() { }
+
+  submit() {
+    this.fruit = "Mango";
+  }
 
 
 }
