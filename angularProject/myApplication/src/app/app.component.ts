4f92bc7ec395c816569e5c1dc122dc8b8e8e6d09
pbhapkar17 <poonambhapkar34@gmail.com>
2023-12-28 22:25:01 +0530

    two way db

--- angularProject/myApplication/src/app/app.component.ts
+++ angularProject/myApplication/src/app/app.component.ts
@@ -13,21 +13,26 @@ export class AppComponent {
   b: number = 90;
   c!: number;
   city: string = "Pune";
+  //db >>defins communication between a component and the DOM. 
+  //It is a technique to link  data to dom
   //1. one way db 2. two way db
-  //1. one way db
+  //1. one way db>> One way databinding is a simple one way communication where  template is changed when we make changes incontrol.
   //1.string interpolation>>{{}} >>syntax {{propertyName}}
   //2.property binding >>[]>> [attribute]="proprtyName"
   //3. event binding>>()>>on event will bind data to the property
-  //Two way data bind >> [()]>> [(ngModel)]="propertyName"
+  //Two way data bind >> [()]>> [(ngModel)]="propertyName">>In two-way databinding, automatic synchronization of data happens between the Model and the View.
   myName = "Poonam";
   isDisable = false;
   fruit!:string;
-
+  favFruit = "Mango";
   constructor() { }
 
   submit() {
     this.fruit = "Mango";
   }
-
+  inputBoxValue(data:any){
+   console.log(data.target.value);
+   this.myName = data.target.value
+  }
 
 }
