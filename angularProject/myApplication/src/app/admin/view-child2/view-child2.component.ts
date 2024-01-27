b3f7b84cf2b9d285279dbbddbc314c4104324c7b
pbhapkar17 <poonambhapkar34@gmail.com>
2024-01-27 16:35:43 +0530

    view

---
+++ angularProject/myApplication/src/app/admin/view-child2/view-child2.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-child2',
+  templateUrl: './view-child2.component.html',
+  styleUrls: ['./view-child2.component.css']
+})
+export class ViewChild2Component {
+  cityName= 'Pune';
+
+  test(){
+    
+  }
+}
