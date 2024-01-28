4ce15612e4132afe7d29b753ed664ff5fc612c7a
pbhapkar17 <poonambhapkar34@gmail.com>
2024-01-28 17:59:30 +0530

    lfh

--- angularProject/myApplication/src/app/child/child.component.ts
+++ angularProject/myApplication/src/app/child/child.component.ts
@@ -9,8 +9,61 @@ export class ChildComponent {
   @Input() cityFromParent:any;
   @Output() dataFromChild = new EventEmitter<any>() ;
   state="MH-09";
-
+  
+  constructor(){
+    console.log('contrustor calling');
+    
+  }
   dataFromChildParent(){
     this.dataFromChild.emit(this.state);
   }
+
+  ngOnInit(){
+    //once
+    //oninit trigger first if compo dont have inputbound property
+    //it will trigger only once at the time of compo initialization.
+    console.log('onInit calling...');
+    
+  }
+
+  ngOnChanges(){
+    //onChanges lf hook called first if compo having input bound property. 
+    //also it wil repeatadly trigger omn every change of input bund property.
+    console.log('onChanges calling...');
+
+  }
+
+  ngDoCheck(){
+    //it will trigger very first time after intialisation of ngOnChjanges lfh 
+    //repeatatly trigger on every change of onChanges lfh
+    console.log('do check calling');
+    
+  }
+
+  ngAfterContentInit(){
+     //once
+console.log('ngAfterContentInit calling');
+
+  }
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked calling');
+  }
+   
+  ngAfterViewInit(){
+     //once
+    console.log('ngAfterViewInit calling');
+  }
+
+  ngAfterViewChecked(){
+    console.log('ngAfterViewChecked calling');
+  }
+
+  ngOnDestroy(){
+     //once
+     console.log('ngOnDestroy');
+     
+  }
+
+
+
 }  
